e66c46ae7e80f1d53b485534e98df3de657d380c
Justin62913 <justin62913@gmail.com>
2023-01-11 15:26:43 -0700

    Added Navigation and syles to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 
-import CategoriesDirectory from "./components/categories-directory/categories.directory";
 
 
+
+ function Shop() {
+     return (
+         <h1>I am the shop page.</h1>
+     )
+ }
 function App() {
   return (
-      <CategoriesDirectory />
+      <Routes>
+          <Route path={'/'} element={<Navigation />}>
+              <Route index element={<Home />} />
+              <Route path={'shop'} element={<Shop />} />
+          </Route>
+      </Routes>
+
   );
 }
 
